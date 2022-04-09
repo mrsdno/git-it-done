c5109bf765adcd062f30171077020bc11a3e44d7
mrsdno <christine@dno.dev>
2022-04-09 09:33:20 -0400

    complete display of user repos and error handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -8,11 +8,20 @@ var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     // make a request to the url
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data){
-            displayRepos(data, user);
-        })
-    });
+    fetch(apiUrl).
+    then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data){
+                displayRepos(data, user);
+            })
+        }
+        else {
+            alert("Error: Github User Not Found");
+        }
+    })
+    .catch(function(error){
+        alert("Unable to connect to GitHub");
+    })
 };
 
 var formSubmitHandler = function(event) {
@@ -30,6 +39,10 @@ var formSubmitHandler = function(event) {
 };
 
 var displayRepos = function(repos, searchTerm) {
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No respositories found.";
+        return;
+    }
     // clear old content
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
