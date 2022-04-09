07e21b0e10bea9fac117e3520e24b73ee6b64260
mrsdno <christine@dno.dev>
2022-04-09 10:51:07 -0400

    set up list of repo issues using hard-code

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
 var getRepoIssues = function(repo) {
     console.log(repo);
     var apiUrl = "https://api.github.com/repos/" + repo +"/issues?direction=asc";
@@ -7,7 +9,12 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data){
                 displayIssues(data);
-            })
+
+                // check if API has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
+            });
         }
         else {
             alert("There as a problem with your request!");
@@ -51,4 +58,18 @@ var displayIssues = function(issues) {
     }
 }
 
-getRepoIssues("mrsdno/christines-portfolio");
+var displayWarning = function(repo) {
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    // set up link to visit
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+}
+
+getRepoIssues("facebook/react");
